13c3d25cbcfb721cbfa721a9428ca96e81faefb0
b18047 <e1b18047@st.oit.ac.jp>
2020-10-06 16:15:56 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z0213/doraemon/DoraemonApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z0213.doraemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoraemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoraemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0213/doraemon/DoraemonApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0213.doraemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoraemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
